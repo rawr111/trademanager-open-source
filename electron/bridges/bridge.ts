1fd99c62e41e2bac4e8cfc4a18af413a3b6ea348
rawr111 <70286254+rawr111@users.noreply.github.com>
2023-06-24 16:38:48 +0300

    compilation errors fixes

--- electron/bridges/bridge.ts
+++ electron/bridges/bridge.ts
@@ -1,13 +1,11 @@
 import mainApi from './mainBridge';
 import confirmationsApi from './confirmationsBridge';
 import askFamilyPinApi from './askFamilyPinBridge';
-import authApi from './authBridge';
 import errorApi from './errorWindowBridge';
 
 export default {
     mainApi: mainApi,
     confirmationsApi: confirmationsApi,
     askFamilyPinApi: askFamilyPinApi,
-    authApi: authApi,
     errorApi: errorApi
 };
